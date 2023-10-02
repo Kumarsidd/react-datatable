e90a9fbc6a33a4dd82b5ccb12a542882c6aa53cc
kumarsidd <kumarsiddhesh159@gmail.com>
2023-10-02 22:45:07 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState(0);
+
+  const fetchData = () => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((res) => setData(res.products))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(data, "check");
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <input type="checkbox" />
+            </th>
+            <th>
+              Id <span id="triangleDown"></span>
+            </th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Brand</th>
+            <th>Category</th>
+            <th>Rating</th>
+            <th>Price</th>
+            <th>Discount</th>
+            <th>Stock</th>
+            <th>Thumbnail</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.length > 0 &&
+            data.map((val) => (
+              <tr key={val.id}>
+                <td>
+                  <input type="checkbox" />
+                </td>
+                <td>{val.id} </td>
+                <td>{val.title}</td>
+                <td>{val.description}</td>
+                <td>{val.brand}</td>
+                <td>{val.category}</td>
+                <td>{val.rating}</td>
+                <td>{val.price}</td>
+                <td>{val.discountPercentage}</td>
+                <td>{val.stock}</td>
+                <td>
+                  <img src={val.thumbnail} height={40} width={50} />
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default App;
